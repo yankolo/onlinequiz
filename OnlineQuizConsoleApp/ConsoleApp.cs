56f82247e3126cc24780570a2a57db963c3b0244
Yanik Kolomatski <yankolo1234@gmail.com>
2018-05-12 19:39:00 -0400

    Added Skeleton Classes

--- OnlineQuizConsoleApp/ConsoleApp.cs
+++ OnlineQuizConsoleApp/ConsoleApp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace OnlineQuizConsoleApp
 {
@@ -10,7 +11,18 @@ namespace OnlineQuizConsoleApp
     {
         public static void Main(string[] args)
         {
-              
+            XElement userXml = XElement.Load("users.xml");
+            var users = from user in userXml.Elements()
+                        select new
+                        {
+                            Username = user.Attribute("userId").Value.Trim(),
+                            FirstName = user.Attribute("firstName").Value.Trim(),
+                            LastName = user.Attribute("lastName").Value.Trim()
+                        };
+            foreach(var usr in users)
+            {
+                Console.Write(usr.Username + " " + usr.FirstName + " " + usr.LastName); 
+            }
         }
     }
 }
