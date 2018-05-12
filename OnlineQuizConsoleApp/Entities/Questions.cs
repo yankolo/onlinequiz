d41432eb1afc7ea3964e34c94bd93d4483a06788
Yanik Kolomatski <yankolo1234@gmail.com>
2018-05-12 19:47:22 -0400

    Add Question Class

--- OnlineQuizConsoleApp/Entities/Questions.cs
+++ OnlineQuizConsoleApp/Entities/Questions.cs
@@ -8,5 +8,13 @@ namespace OnlineQuizConsoleApp.Entities
 {
     public class Questions
     {
+        public String QuestionName { get; set; }
+        public Categories Categerory { get; set; }
+        public String Option1 { get; set; }
+        public String Option2 { get; set; }
+        public String Option3 { get; set; }
+        public String option4 { get; set; }
+        public String RightOption { get; set; }
+        public Users Username { get; set; }
     }
 }
