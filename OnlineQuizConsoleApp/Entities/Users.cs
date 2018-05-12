7844641d2d00f3ac2a054b91294c8260e741ef50
Yanik Kolomatski <yankolo1234@gmail.com>
2018-05-12 19:58:16 -0400

    Add Users Class

--- OnlineQuizConsoleApp/Entities/Users.cs
+++ OnlineQuizConsoleApp/Entities/Users.cs
@@ -8,5 +8,9 @@ namespace OnlineQuizConsoleApp.Entities
 {
     public class Users
     {
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
     }
 }
